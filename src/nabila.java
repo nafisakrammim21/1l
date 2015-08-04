4cf00f3740d99d0dd9a03349eb32d02f461e1534
nafisakrammim21 <rashikhasnat@ymail.com>
2015-08-04 12:23:01 +0600

    massage

---
+++ src/nabila.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+
+public class nabila {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+      ArrayList<String> list = new ArrayList<String>();
+      list.add("aaa");
+      list.add("bbbb");
+      list.add("ccccc");
+      
+	}
+
+}
